d5485575d7e2c64bd1d8f94093d444572f3a07aa
Shashi <sharmashashi940@gmail.com>
2020-10-13 08:26:19 +0545

    api consume

---
+++ lib/screens/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String url = "https://jsonplaceholder.typicode.com/posts";
+  Widget loading = Container();
+  String response = "No response yet";
+  List<Widget> items = List();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text("api consume"),
+      ),
+      body: ListView(
+        children: [
+          loading,
+          MaterialButton(
+            onPressed: () async {
+              setState(() {
+                loading = LinearProgressIndicator();
+              });
+
+              http.post(url).then((res) {
+                print(res.body);
+              });
+
+              List<Widget> temp = await getPost();
+              setState(() {
+                items = temp;
+                loading = Container();
+              });
+            },
+            child: Text("get"),
+            color: Colors.white,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: items,
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<List<Widget>> getPost() async {
+    List<Widget> itemList = List();
+    http.Response res = await http.get(url);
+    List converted = convert.jsonDecode(res.body);
+
+    for (Map<String, dynamic> each in converted) {
+      itemList.add(eachElement(each));
+    }
+
+    return itemList;
+  }
+
+  Widget eachElement(Map<String, dynamic> body) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(blurRadius: 2, spreadRadius: 0.5, offset: Offset(1, 1))
+      ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            body['title'],
+            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            body['body'],
+            style: GoogleFonts.sansita(color: Colors.red),
+          )
+        ],
+      ),
+    );
+  }
+}
