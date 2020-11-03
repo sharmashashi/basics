87779b41d13eee517a9afa73fa94462e3a4c307c
Shashi <sharmashashi940@gmail.com>
2020-11-03 08:39:20 +0545

    crud operation

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,10 +8,69 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _editingController = TextEditingController();
+  String data = "Initial";
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    Firebase.initializeApp();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Firebase CRUD",
+        ),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _editingController,
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text("Create"),
+            onPressed: () async {
+              FirebaseFirestore ins = FirebaseFirestore.instance;
+              Map<String, dynamic> data = {
+                "typedText": _editingController.text
+              };
+              // await ins.collection("message").doc("first").set(data);
+              await ins.collection("message").doc("second").set(data);
+            },
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text("Read"),
+            onPressed: () async {
+              FirebaseFirestore ins = FirebaseFirestore.instance;
+              DocumentSnapshot snapshot =
+                  await ins.collection("message").doc("second").get();
+              String _data = snapshot.data().toString();
+              setState(() {
+                data = _data;
+              });
+            },
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text("Update"),
+            onPressed: () async {
+              FirebaseFirestore ins = FirebaseFirestore.instance;
+              Map<String, dynamic> data = {
+                "datetime": DateTime.now().toIso8601String()
+              };
+              await ins.collection("message").doc("first").update(data);
+            },
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            child: Text("Delete"),
+            onPressed: () async {
+              FirebaseFirestore ins = FirebaseFirestore.instance;
+              await ins.collection("message").doc("second").delete();
+            },
+          ),
+          Text("Data from firestore: $data")
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-
-
 import 'package:firstproject/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 }
