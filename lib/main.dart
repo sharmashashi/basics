2d18d72634d22b41b7f9b8da3974977d39e8b149
Shashi <sharmashashi940@gmail.com>
2020-11-04 07:43:58 +0545

    used stream builder to listen doc change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firstproject/screens/auto_update_list.dart';
 import 'package:firstproject/screens/home.dart';
 import 'package:firstproject/screens/streams_example.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: StreamExample()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+      MaterialApp(debugShowCheckedModeBanner: false, home: AutoUpdateList()));
 }
