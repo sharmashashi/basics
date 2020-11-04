cdb2c654b44f197609eb4e63408ad935173ce4aa
Shashi <sharmashashi940@gmail.com>
2020-11-04 07:29:05 +0545

    basic stream example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:firstproject/home/home.dart';
+import 'package:firstproject/screens/home.dart';
+import 'package:firstproject/screens/streams_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: StreamExample()));
 }
